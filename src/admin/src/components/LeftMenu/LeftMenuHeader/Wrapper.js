f728abca951319db5874b99a6454f88269cd279c
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2022-01-15 04:29:16 +0500

    new updates

---
+++ src/admin/src/components/LeftMenu/LeftMenuHeader/Wrapper.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import Logo from "../../../assets/images/logo-strapi.png";
+
+const Wrapper = styled.div`
+  background-color: rgb(255, 255, 255);
+  box-shadow: rgb(40 42 49 / 16%) 0px 1px 2px 0px;
+  padding-left: 2rem;
+  height: ${(props) => props.theme.main.sizes.leftMenu.height};
+
+  .leftMenuHeaderLink {
+    &:hover {
+      text-decoration: none;
+    }
+  }
+
+  .projectName {
+    display: block;
+    width: 100%;
+    height: ${(props) => props.theme.main.sizes.leftMenu.height};
+    font-size: 2rem;
+    letter-spacing: 0.2rem;
+    color: $white;
+
+    background-image: url(${Logo});
+    background-repeat: no-repeat;
+    background-position: left center;
+    background-size: auto 2.5rem;
+  }
+`;
+
+Wrapper.defaultProps = {
+  theme: {
+    main: {
+      colors: {
+        leftMenu: {},
+      },
+      sizes: {
+        header: {},
+        leftMenu: {},
+      },
+    },
+  },
+};
+
+Wrapper.propTypes = {
+  theme: PropTypes.object,
+};
+
+export default Wrapper;
